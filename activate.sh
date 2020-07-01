22ac0530d516799584a3df51a090fe3a2b1da11c
Aurélien Rainone <aurelien.rainone@gmail.com>
2020-07-01 17:23:23 +0200

    activate.sh: add $(go env root) to $GO2PATH

--- activate.sh
+++ activate.sh
@@ -1,3 +1,2 @@
 export PATH=/home/aurelien/dev/go/bin:$PATH
-export GO2PATH="$PWD"
-#export GO2PATH="$(go env GOROOT)/src/cmd/go2go/testdata/go2path"
+export GO2PATH="$PWD:$(go env GOROOT)/src/cmd/go2go/testdata/go2path"
